9dcba213f82d43b4b33755637ac617b1b397fb68
khaizinam <84978080+khaizinam@users.noreply.github.com>
2021-10-27 01:18:11 +0700

    27

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,9 +1,8 @@
 // Modal
+
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("cart");
 var close = document.getElementsByClassName("close")[0];
-// tại sao lại có [0] như  thế này bởi vì mỗi close là một html colection nên khi mình muốn lấy giá trị html thì phải thêm [0]. 
-//Nếu mình có 2 cái component cùng class thì khi [0] nó sẽ hiển thị component 1 còn [1] thì nó sẽ hiển thị component 2.
 var close_footer = document.getElementsByClassName("close-footer")[0];
 var order = document.getElementsByClassName("order")[0];
 btn.onclick = function () {
